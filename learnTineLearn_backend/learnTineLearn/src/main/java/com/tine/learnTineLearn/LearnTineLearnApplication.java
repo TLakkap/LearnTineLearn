b4e07f56cca3fbb1ae07038065be9d10014bf0a0
Tiina Lakkapää <tiina.lakkapaa@gmail.com>
2024-12-10 14:31:32 +0200

    .env file

--- learnTineLearn_backend/learnTineLearn/src/main/java/com/tine/learnTineLearn/LearnTineLearnApplication.java
+++ learnTineLearn_backend/learnTineLearn/src/main/java/com/tine/learnTineLearn/LearnTineLearnApplication.java
@@ -2,11 +2,15 @@ package com.tine.learnTineLearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LearnTineLearnApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // Lataa .env-tiedoston
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 
 		SpringApplication.run(LearnTineLearnApplication.class, args);
 	}
