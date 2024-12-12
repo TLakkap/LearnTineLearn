57861a4c91bb5f5bb9a335855c90eb22227aa452
Tiina Lakkapää <tiina.lakkapaa@gmail.com>
2024-12-12 14:58:13 +0200

    read values from .env

--- learnTineLearn_backend/learnTineLearn/src/main/java/com/tine/learnTineLearn/UserDataInit.java
+++ learnTineLearn_backend/learnTineLearn/src/main/java/com/tine/learnTineLearn/UserDataInit.java
@@ -4,6 +4,7 @@ import com.tine.learnTineLearn.model.User;
 import com.tine.learnTineLearn.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,12 +17,15 @@ public class UserDataInit {
         @Autowired
         private PasswordEncoder passwordEncoder;
 
+        @Value("${ADMIN_PWD}")
+        private String password;
+
         @PostConstruct
         public void init() {
             if (userRepository.findByUsername("admin").isEmpty()) {
                 User admin = new User();
                 admin.setUsername("admin");
-                admin.setPassword(passwordEncoder.encode("passwordhere"));
+                admin.setPassword(passwordEncoder.encode(password));
                 userRepository.save(admin);
             }
         }
